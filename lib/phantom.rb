6d56715477da8b5dabb9796549b4d14c59177bbb
Vivek <vivekmiyaniofficial@gmail.com>
2023-01-23 14:27:43 +0530

    Revert "Lazy after_run hook"

--- lib/phantom.rb
+++ lib/phantom.rb
@@ -5,23 +5,15 @@ module Phantom
 
   @processes = []
 
-  def self.register_hook
-    @register_hook ||= begin
-      Minitest.after_run do
-        @processes.each do |process|
-          Process.kill "-TERM", process
-        rescue # rubocop:disable Style/RescueStandardError
-          # do nothing
-        end
-      end
-
-      true
+  Minitest.after_run do
+    @processes.each do |process|
+      Process.kill "-TERM", process
+    rescue # rubocop:disable Style/RescueStandardError
+      # do nothing
     end
   end
 
   def self.start(command, dir:, url:, env: {})
-    register_hook
-
     puts "* Starting #{command}"
     pid = Process.spawn(env, command, chdir: dir, err: NULL, in: NULL, out: NULL, pgroup: true)
 
